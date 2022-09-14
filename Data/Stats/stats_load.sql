ade3575c1dd4a0a12833a969d35bbe78d5778043
AnonymousSigmod2023 <108958308+AnonymousSigmod2023@users.noreply.github.com>
2022-09-14 12:58:24 -0700

    Add files via upload

---
+++ Data/Stats/stats_load.sql
@@ -0,0 +1,8 @@
+\copy badges from 'datasets/stats_simplified/badges.csv' with CSV header;
+\copy comments from 'datasets/stats_simplified/comments.csv' with CSV header;
+\copy users from 'datasets/stats_simplified/users.csv' with CSV header;
+\copy tags from 'datasets/stats_simplified/tags.csv' with CSV header;
+\copy posts from 'datasets/stats_simplified/posts.csv' with CSV header;
+\copy votes from 'datasets/stats_simplified/votes.csv' with CSV header;
+\copy posthistory from 'datasets/stats_simplified/postHistory.csv' with CSV header;
+\copy postlinks from 'datasets/stats_simplified/postLinks.csv' with CSV header;

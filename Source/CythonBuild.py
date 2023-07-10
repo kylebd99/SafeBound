d5e7b51a12eb0b7d7e7e7920bf1a8a6216d144fa
kylebd99 <kylebd99@gmail.com>
2023-07-10 09:44:39 -0700

    Added C++ to cython build files

--- Source/CythonBuild.py
+++ Source/CythonBuild.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 
-setup(ext_modules = cythonize('HistogramUtils.pyx', language_level = "3"))
-setup(ext_modules = cythonize('JoinGraphUtils.pyx', language_level = "3"))
-setup(ext_modules = cythonize('SafeBoundUtils.pyx', language_level = "3"))
-setup(ext_modules = cythonize('PiecewiseConstantFunctionUtils.pyx', language_level = "3"))
+setup(ext_modules = cythonize('HistogramUtils.pyx', language_level = "3", language="c++"))
+setup(ext_modules = cythonize('JoinGraphUtils.pyx', language_level = "3", language="c++"))
+setup(ext_modules = cythonize('SafeBoundUtils.pyx', language_level = "3", language="c++"))
+setup(ext_modules = cythonize('PiecewiseConstantFunctionUtils.pyx', language_level = "3", language="c++"))
